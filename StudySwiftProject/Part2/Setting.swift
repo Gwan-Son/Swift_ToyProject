624cfe3d83dd9ea6139697368d345290481ddf5d
GwanSon <id147369@naver.com>
2023-12-01 01:27:03 +0900

    23.12.01 UPDATE

---
+++ StudySwiftProject/Part2/Setting.swift
@@ -0,0 +1,18 @@
+//
+//  Setting.swift
+//  StudySwiftProject
+//
+//  Created by 심관혁 on 11/30/23.
+//
+
+import SwiftUI
+
+struct Setting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Setting()
+}
