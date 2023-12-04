c53408b3e441c941343829209bced096316c8d8e
GwanSon <id147369@naver.com>
2023-12-05 01:01:14 +0900

    23.12.05 Add

--- StudySwiftProject/ViewController.swift
+++ StudySwiftProject/ViewController.swift
@@ -2,28 +2,26 @@
 //  ViewController.swift
 //  StudySwiftProject
 //
-//  Created by 심관혁 on 11/18/23.
-//
+//  Created by 심관혁 on 12/04/23.
+//  Ch 4. 01. 02. 03.
 
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBOutlet weak var valueLabel: UILabel!
-    
-    @IBOutlet weak var inputField: UITextField!
-    
-    @IBAction func showValue(_ sender: Any) {
-        let name = inputField.text!
-        valueLabel.text = "Hello, \(name)"
-    }
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green // 타입은 UIColor SwiftUI의 Color와 다름
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func didMyButtonTapped(_ sender: Any) {
+        myLabel.text = "Hello, UIKit"
+    }
+    
 
 }
 
