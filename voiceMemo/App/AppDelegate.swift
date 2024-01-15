a55bea9a4b6117f69f05bdb6d347c6d8eb749bce
GwanSon <id147369@naver.com>
2024-01-15 17:53:11 +0900

    ADD 24.01.15

--- voiceMemo/App/AppDelegate.swift
+++ voiceMemo/App/AppDelegate.swift
@@ -5,6 +5,13 @@
 
 import UIKit
 
-class AppDelegate {
-  
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var notifitionDelegate = NotificationDelegate()
+    
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = notifitionDelegate
+        return true
+    }
 }
