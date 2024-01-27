0527d10f43c6cec311c029d891f720a2d56d8d2c
GwanSon <id147369@naver.com>
2024-01-27 20:40:13 +0900

    ADD 24.01.27

---
+++ LMessenger/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  LMessenger
+//
+//  Created by 심관혁 on 1/25/24.
+//
+
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
---
+++ LMessenger/Service/AuthenticationService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationService.swift
+//  LMessenger
+//
+//  Created by 심관혁 on 1/25/24.
+//
+
+import Foundation
