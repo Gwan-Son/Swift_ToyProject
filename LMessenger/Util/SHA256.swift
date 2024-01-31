a3b1f47e50809528ed41277659d622ae28a3ad4d
GwanSon <id147369@naver.com>
2024-01-31 18:30:27 +0900

    ADD 24.01.31

---
+++ LMessenger/Util/SHA256.swift
@@ -0,0 +1,19 @@
+//
+//  SHA256.swift
+//  LMessenger
+//
+//  Created by 심관혁 on 1/31/24.
+//
+
+import Foundation
+import CryptoKit
+
+func sha256(_ input: String) -> String {
+    let inputData = Data(input.utf8)
+    let hashedData = SHA256.hash(data: inputData)
+    let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+    }.joined()
+    
+    return hashString
+}
