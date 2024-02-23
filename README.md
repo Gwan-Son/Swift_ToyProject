9b4ce80049b140af02e1a611861f82abf2f2924d
GwanSon <id147369@naver.com>
2024-02-23 18:12:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SymbolRoller
+ StoryBoard를 이용한 간단한 SymbolRoller
---
+++ SymbolRoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SymbolRoller
+//
+//  Created by 심관혁 on 2/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
