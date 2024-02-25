e57caa9fdd1b5d3c1254ac348a5cd50365c795f6
GwanSon <id147369@naver.com>
2024-02-25 17:15:41 +0900

    update 24.02.25

---
+++ ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by 심관혁 on 2/25/24.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ProfileImage: UIImageView!
+    @IBOutlet weak var ProfileName: UILabel!
+    @IBOutlet weak var Message: UILabel!
+    @IBOutlet weak var Date: UILabel!
+}
