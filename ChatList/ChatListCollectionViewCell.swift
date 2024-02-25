cb5a95bd1cad184a2be811427b44278db6428c59
GwanSon <id147369@naver.com>
2024-02-26 02:15:47 +0900

    final update 24.02.26

--- ChatList/ChatListCollectionViewCell.swift
+++ ChatList/ChatListCollectionViewCell.swift
@@ -13,4 +13,29 @@ class ChatListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var ProfileName: UILabel!
     @IBOutlet weak var Message: UILabel!
     @IBOutlet weak var Date: UILabel!
+    
+    // StoryBoard에서 Cell을 꺼내는 함수
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        ProfileImage.layer.cornerRadius = 40
+    }
+    
+    func configure(_ chat: Chat) {
+        ProfileImage.image = UIImage(named: chat.name)
+        ProfileName.text = chat.name
+        Message.text = chat.chat
+        Date.text = changeDateFormat(dateString: chat.date)
+    }
+    
+    func changeDateFormat(dateString: String) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        if let date = formatter.date(from: dateString) {
+            formatter.dateFormat = "M/d"
+            return formatter.string(from: date)
+        } else{
+            return ""
+        }
+    }
 }
