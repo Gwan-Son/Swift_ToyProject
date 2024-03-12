d63effb4dc966d91020a96e6d40962e15b5343fd
GwanSon <id147369@naver.com>
2024-03-12 16:06:56 +0900

    update 24.03.12
    CollectionViewController를 활용하여 Reminder 리스트 구현

--- Reminder/ReminderListViewController.swift
+++ Reminder/ReminderListViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    var dataSource: DataSource!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        /* snapshot에 Item을 추가
+         var reminderTitles = [String]()
+         for reminder in Reminder.sampleData {
+            reminderTitles.append(reminder.title)
+         }
+         snapshot.appendItems(reminderTitles)
+         */
+        // 위의 코드 축약형
+        snapshot.appendItems(Reminder.sampleData.map {$0.title})
+        dataSource.apply(snapshot)
+        
+        collectionView.dataSource = dataSource
     }
     
     private func listLayout() -> UICollectionViewCompositionalLayout {
