f1ca1601a583c7d33795adbe9c9f126c2bb07dbf
GwanSon <id147369@naver.com>
2024-03-13 03:29:25 +0900

    update 24.03.13
    Reminder의 백그라운드와 마감일 표시를 구현하였습니다.

---
+++ Reminder/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Reminder
+//
+//  Created by 심관혁 on 3/13/24.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(red: 1.0, green: 0.8, blue: 0.9, alpha: 1.0)
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+}
