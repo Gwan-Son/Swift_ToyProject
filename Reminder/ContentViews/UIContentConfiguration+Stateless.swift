6ff253728a44750ab99c701f613455ced36064d9
GwanSon <id147369@naver.com>
2024-03-17 04:17:17 +0900

    update 24.03.17
    ContenView 프로토콜을 준수하여 TextFieldContentView를 수정하였습니다.

---
+++ Reminder/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Reminder
+//
+//  Created by 심관혁 on 3/17/24.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
