45e6e5395317be130e9a464338694175fb715b60
GwanSon <id147369@naver.com>
2024-04-08 18:08:27 +0900

    update 24.04.08

---
+++ DailySpark/Extensions/Color+Extenstion.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extenstion.swift
+//  DailySpark
+//
+//  Created by 심관혁 on 4/8/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary_green = UIColor(named: "primaryGreen")
+}
