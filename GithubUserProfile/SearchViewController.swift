02c5196b13f7f341f7927f564255466ebbe8d5dd
GwanSon <id147369@naver.com>
2024-04-16 16:53:02 +0900

    update 24.04.16
    MVVM 패턴을 적용하였습니다.

--- GithubUserProfile/SearchViewController.swift
+++ GithubUserProfile/SearchViewController.swift
@@ -10,10 +10,12 @@ import Kingfisher
 
 class UserProfileViewController: UIViewController {
     
-    let network = NetworkService(configuration: .default)
+//    let network = NetworkService(configuration: .default)
     
-    @Published private(set) var user: UserProfile?
+//    @Published private(set) var user: UserProfile?
     var subscription = Set<AnyCancellable>()
+    
+    var viewModel: SearchViewModel!
     
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -23,7 +25,7 @@ class UserProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel = SearchViewModel(network: NetworkService(configuration: .default), selectedUser: nil)
         setupUI()
         embedSearchControl()
         bind()
@@ -45,29 +47,16 @@ class UserProfileViewController: UIViewController {
     }
     
     private func bind() {
-        $user
+        viewModel.selectedUser
             .receive(on: RunLoop.main)
-            .sink { [unowned self] result in
-                self.update(result)
+            .sink { [unowned self] _ in
+                self.nameLabel.text = self.viewModel.name
+                self.loginLabel.text = self.viewModel.login
+                self.followerLabel.text = self.viewModel.followers
+                self.followingLabel.text = self.viewModel.following
+                
+                self.thumbnail.kf.setImage(with: self.viewModel.imageURL)
             }.store(in: &subscription)
-    }
-    
-    private func update(_ user: UserProfile?) {
-        guard let user = user else {
-            self.nameLabel.text = "n/a"
-            self.loginLabel.text = "n/a"
-            self.followerLabel.text = ""
-            self.followingLabel.text = ""
-            self.thumbnail.image = nil
-            return
-        }
-        self.nameLabel.text = user.name
-        self.loginLabel.text = user.login
-        self.followerLabel.text = "follower: \(user.followers)"
-        self.followingLabel.text = "following: \(user.following)"
-        
-        self.thumbnail.kf.setImage(with: user.avatarUrl)
-        
     }
 }
 
@@ -80,64 +69,6 @@ extension UserProfileViewController: UISearchResultsUpdating {
 extension UserProfileViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
-        
-        // Resource
-//        let base = "https://api.github.com/"
-//        let path = "users/\(keyword)"
-//        let params: [String: String] = [:]
-//        let header: [String: String] = ["Content-Type": "application/json"]
-//        var urlComponents = URLComponents(string: base + path)!
-//        let queryItems = params.map { (key: String, value: String) in
-//            return URLQueryItem(name: key, value: value)
-//        }
-//        urlComponents.queryItems = queryItems
-//        
-//        var request = URLRequest(url: urlComponents.url!)
-//        header.forEach { (key: String, value: String) in
-//            request.addValue(value, forHTTPHeaderField: key)
-//        }
-        let resource = Resource<UserProfile>(
-            base: "https://api.github.com/users/", 
-            path: "\(keyword)",
-            params: [:], 
-            header: ["Content-Type": "application/json"])
-        
-        
-        // NetworkService
-        network.load(resource)
-            .receive(on: RunLoop.main)
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    self.user = nil
-                case .finished: break
-                }
-            } receiveValue: { user in
-                self.user = user
-            }.store(in: &subscription)
-
-//        URLSession.shared
-//            .dataTaskPublisher(for: request)
-//            .tryMap { result -> Data in
-//                guard let response = result.response as? HTTPURLResponse, (200..<300).contains(response.statusCode) else {
-//                    let response = result.response as? HTTPURLResponse
-//                    let statusCode = response?.statusCode ?? -1
-//                    throw NetworkError.responseError(statusCode: statusCode)
-//                }
-//                return result.data
-//            }
-//            .decode(type: UserProfile.self, decoder: JSONDecoder())
-//            .receive(on: RunLoop.main)
-//            .sink { completion in
-//                print("completion: \(completion)")
-//                
-//                switch completion {
-//                case .failure(let error):
-//                    self.user = nil
-//                case .finished: break
-//                }
-//            } receiveValue: { user in
-//                self.user = user
-//            }.store(in: &subscription)
+        viewModel.search(keyword: keyword)
     }
 }
