fb6fb238541b15aa7cc437f40128057b1bf6c4ea
GwanSon <id147369@naver.com>
2024-04-19 13:41:16 +0900

    Initial commit

---
+++ EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by 심관혁 on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
